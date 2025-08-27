3fa89ddba51a980b861f1ab6a23bc3942aff30ba
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-08-28 00:52:23 +0630

    Update comment.config.js

--- conf/comment.config.js
+++ conf/comment.config.js
@@ -12,7 +12,7 @@ module.exports = {
   COMMENT_ARTALK_CSS: '', // ArtalkServert css cdn
 
   // twikoo
-  COMMENT_TWIKOO_ENV_ID: process.env.NEXT_PUBLIC_COMMENT_ENV_ID || '', // TWIKOO后端地址 腾讯云环境填envId；Vercel环境填域名，教程：https://tangly1024.com/article/notionnext-twikoo
+  COMMENT_TWIKOO_ENV_ID: process.env.NEXT_PUBLIC_COMMENT_ENV_ID || 'https://my-twikoo-backen.vercel.app', // TWIKOO后端地址 腾讯云环境填envId；Vercel环境填域名，教程：https://tangly1024.com/article/notionnext-twikoo
   COMMENT_TWIKOO_COUNT_ENABLE: false, // 博客列表是否显示评论数
   COMMENT_TWIKOO_CDN_URL:
     'https://s4.zstatic.net/npm/twikoo@1.6.44/dist/twikoo.min.js', // twikoo客户端cdn
