62ed7c236ff02c5402c8d767c3be580f2d073137
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-08-30 05:09:34 +0630

    Update NotionPage.js

--- components/NotionPage.js
+++ components/NotionPage.js
@@ -22,6 +22,7 @@ const CUSTOM_COMPONENTS_MAP = {
   '/components/PaiXuTi.js': dynamic(() => import('@/components/PaiXuTi'), { ssr: false }),
   '/components/PinyinHanziQuiz.js': dynamic(() => import('@/components/PinyinHanziQuiz'), { ssr: false }),
   '/components/LianXianTi.js': dynamic(() => import('@/components/LianXianTi'), { ssr: false }),
+  '/components/AiCorrector.js': dynamic(() => import('@/components/AiCorrector'), { ssr: false }),
   '/components/BeiDanCi.js': dynamic(() => import('@/components/BeiDanCi'), { ssr: false }), // 背单词组件
   // ======================================
   
