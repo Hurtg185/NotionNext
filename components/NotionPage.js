666f5133b645278c09c95b91ef74abff5615ee27
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-08-30 10:09:07 +0630

    Update NotionPage.js

--- components/NotionPage.js
+++ components/NotionPage.js
@@ -20,6 +20,8 @@ const CUSTOM_COMPONENTS_MAP = {
   // === 我们目前已开发的交互式题目组件 ===
   '/components/XuanZeTi.js': dynamic(() => import('@/components/XuanZeTi'), { ssr: false }),
   '/components/PaiXuTi.js': dynamic(() => import('@/components/PaiXuTi'), { ssr: false }),
+  '/components/ExternalTextToSpeech.js': dynamic(() => import('@/components/ExternalTextToSpeech'), { ssr: false }),
+  '/components/AiChatAssistant.js': dynamic(() => import('@/components/AiChatAssistant'), { ssr: false }),
   '/components/PinyinHanziQuiz.js': dynamic(() => import('@/components/PinyinHanziQuiz'), { ssr: false }),
   '/components/LianXianTi.js': dynamic(() => import('@/components/LianXianTi'), { ssr: false }),
   '/components/AiCorrector.js': dynamic(() => import('@/components/AiCorrector'), { ssr: false }),
