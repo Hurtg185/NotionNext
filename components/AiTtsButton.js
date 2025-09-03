0311778a6077b98000658f41a7d3eb4ad23e7d0d
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-09-03 22:39:15 +0630

    Update AiTtsButton.js

--- components/AiTtsButton.js
+++ components/AiTtsButton.js
@@ -42,7 +42,7 @@ const AiTtsButton = ({ text, ttsSettings = {} }) => {
     ttsEngine = TTS_ENGINE.THIRD_PARTY,
     thirdPartyTtsVoice = 'zh-CN-XiaoxiaoMultilingualNeural',
     systemTtsVoiceURI = '',
-    ttsRate = -68, // [核心修正] 接收语速参数，默认为0 (正常语速)
+    ttsRate = -30, // [核心修正] 接收语速参数，默认为0 (正常语速)
   } = ttsSettings;
 
   useEffect(() => {
@@ -146,7 +146,7 @@ const AiTtsButton = ({ text, ttsSettings = {} }) => {
       case 'loading':
         return <svg className="animate-spin h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>;
       case 'playing':
-        return <i className="fas fa-volume-mute h-5 w-5 flex items-center justify-center"></i>;
+        return <i className="fas fa-pause-circle h-5 w-5 flex items-center justify-center"></i>; // 使用 Font Awesome 的暂停图标
       case 'paused':
       case 'idle':
       default:
