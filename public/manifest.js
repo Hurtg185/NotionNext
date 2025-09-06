65ba4dd45c162f25851934d0b03d394b06665b14
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-09-07 00:31:33 +0630

    Create manifest.js

---
+++ public/manifest.js
@@ -0,0 +1,27 @@
+{
+  "name": "中文培训中心",
+  "short_name": "学中文",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff",
+  "description": "缅甸人学中文的网站和AI助手。",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "/favicon.ico",
+      "sizes": "48x48",
+      "type": "image/x-icon"
+    },
+    {
+      "src": "/images/icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/images/icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
