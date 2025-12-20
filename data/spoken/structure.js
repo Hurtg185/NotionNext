5b01c30cb453794a6ec04cdda37df727b9be201b
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-12-20 15:55:36 +0630

    口语数据层

---
+++ data/spoken/structure.js
@@ -0,0 +1,18 @@
+export const spokenBooks = [
+  { 
+    id: '10k', 
+    title: '日常高频10000句', 
+    icon: '🔥', 
+    tag: 'Daily', 
+    file: 'daily10k', 
+    image: 'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=400' 
+  },
+  { 
+    id: 'factory', 
+    title: '服装厂实战会话', 
+    icon: '🏭', 
+    tag: 'Work', 
+    file: 'factory', 
+    image: 'https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?w=400' 
+  }
+];
