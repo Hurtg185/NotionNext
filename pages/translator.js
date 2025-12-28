031e7ed72e4df53f8bab09c7d45b037b8daadc26
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-12-28 11:17:46 +0630

    Update translator.js

--- pages/translator.js
+++ pages/translator.js
@@ -1,4 +1,5 @@
-// pages/index.tsx
+// pages/translator.js
+
 import Head from 'next/head';
 import TranslatorChat from '../components/TranslatorChat';
 
@@ -7,4 +8,13 @@ export default function Home() {
     <>
       <Head>
         <title>中缅翻译 - AI 智能翻译引擎</title>
-        <meta name="description" content="中缅双语
+        <meta name="description" content="中缅双语智能翻译" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <TranslatorChat />
+      </main>
+    </>
+  );
+}
