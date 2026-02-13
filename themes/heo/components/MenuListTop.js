58147910456edce8a57ae2069f4d6b1eda661db5
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2026-02-13 19:19:13 +0630

    Update MenuListTop.js

--- themes/heo/components/MenuListTop.js
+++ themes/heo/components/MenuListTop.js
@@ -3,25 +3,8 @@ import { useGlobal } from '@/lib/global'
 import CONFIG from '../config'
 import { MenuItemDrop } from './MenuItemDrop'
 
-const ACCOUNT_KEYWORDS = ['账号', '账户', 'account', 'profile', '个人中心', 'user center']
-const ACCOUNT_HREF_KEYWORDS = ['/account', '/profile', '/user', '/login', '/dashboard', '/me']
-
-const isAccountLikeLink = (link) => {
-  if (!link) return false
-
-  const name = String(link.name || '').toLowerCase()
-  const href = String(link.href || '').toLowerCase()
-  const icon = String(link.icon || '').toLowerCase()
-
-  const nameHit = ACCOUNT_KEYWORDS.some((k) => name.includes(k.toLowerCase()))
-  const hrefHit = ACCOUNT_HREF_KEYWORDS.some((k) => href.includes(k))
-  const iconHit = icon.includes('user')
-
-  return nameHit || hrefHit || iconHit
-}
-
-export const MenuListTop = (props) => {
-  const { customNav, customMenu, hideAccountButton } = props
+export const MenuListTop = props => {
+  const { customNav, customMenu } = props
   const { locale } = useGlobal()
 
   let links = [
@@ -52,13 +35,9 @@ export const MenuListTop = (props) => {
     links = links.concat(customNav)
   }
 
-  // 如果开启自定义菜单，则覆盖默认菜单
+  // 如果 开启自定义菜单，则覆盖Page生成的菜单
   if (siteConfig('CUSTOM_MENU')) {
-    links = customMenu || []
-  }
-
-  if (hideAccountButton) {
-    links = (links || []).filter((item) => !isAccountLikeLink(item))
+    links = customMenu
   }
 
   if (!links || links.length === 0) {
@@ -66,8 +45,15 @@ export const MenuListTop = (props) => {
   }
 
   return (
-    <nav id='nav-mobile' className='leading-8 justify-center font-light w-full flex'>
-      {links.map((link) => link && link.show && <MenuItemDrop key={link.id || link.href || link.name} link={link} />)}
-    </nav>
+    <>
+      <nav
+        id='nav-mobile'
+        className='leading-8 justify-center font-light w-full flex'>
+        {links?.map(
+          (link, index) =>
+            link && link.show && <MenuItemDrop key={index} link={link} />
+        )}
+      </nav>
+    </>
   )
-  }
+}
